8040973742a4730449bfaff73c2b2bfdf75402e2
Yerzhan <yerzhan.aday@gmail.com>
2023-03-02 11:14:24 +0600

    095

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,22 +3,25 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete, onToggleImportant, onToggleLiked}) => {
 
     const elements = posts.map(item => {
 
         const {id, ...itemProps} = item;
 
         return (
-            <li ket={id} className='list-group-item'>
+            <li key={id} className='list-group-item'>
                 <PostListItem
                 // label={item.label}
                 // important={item.important}
-                {...itemProps} />
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}
+                    onToggleImportant={() => onToggleImportant(id)}
+                    onToggleLiked={() => onToggleLiked(id)} />
             </li>
         )
     })
-    
+
     return (
         <ul className = "app-list list-group">
             {elements}
